1fd9117304ea3a6d6109054bcc846d8f35822b14
Edwin <epenangsang@binar.co.id>
2023-08-20 21:43:34 +0700

    add shipping service

---
+++ build_image_shipping.sh
@@ -0,0 +1,8 @@
+#melakukan build image
+docker build -t mredw/shipping-service:latest .
+#melihat image list
+docker image ls
+#login docker hub
+echo $ACCESS_TOKEN | docker login -u mredw --password-stdin
+#push ke docker hub
+docker push mredw/shipping-service:latest
